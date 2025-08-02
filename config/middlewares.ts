addce20c731eaa23cc0e401e8e7a5ab0c85aa628
Hamza Sa'eed Ralsan Sangare <saeedralsans@gmail.com>
2025-08-02 22:09:47 +0100

    Add CORS origin for GitHub Pages

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,20 +1,23 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://localhost:5174'], // frontend dev server
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization'],
-      credentials: true,
+    'strapi::logger',
+    'strapi::errors',
+    'strapi::security',
+    {
+        name: 'strapi::cors',
+        config: {
+            origin: [
+                'http://localhost:5174',
+                'https://hamza-hub320.github.io'
+            ],
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            headers: ['Content-Type', 'Authorization'],
+            credentials: true,
+        },
     },
-  },
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
 ];
